ae39e27646bdfa10663c7a99b15e6ebb84d14445
followingscl <2267081957@qq.com>
2017-04-29 15:31:07 +0800

    Changes to be committed:

--- head.php
+++ head.php
@@ -46,29 +46,6 @@
 	<div class="contain">
 		<div id="navbar" class="navbar navbar-default">
         </div>
-        <div class="row">
-          <div class="col-xs-12 col-sm-12" style="width: 100%;height: 250px;background-image: url('./assets/img/home2.jpg');background-size: 100% 100%;">
-          </div>
-        </div>
-        <div class="row" id="row2">
-          <div class="col-xs-2 col-sm-2 col-md-offset-2">
-            <a href="#">
-              <img src="./assets/img/查询.png">
-            </a>
-          </div>
-          <div class="col-xs-2 col-sm-2 col-md-offset-2" >
-            <a href="#">
-              <img src="./assets/img/统计.png" >
-            </a>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-6 col-md-offset-3">
-            <a href="#">
-              <img src="./assets/img/月报表.png">
-            </a>
-          </div>
-        </div>  
 			<!--div class="navbar-container" id="navbar-container">
 			</div-->
     </div>
