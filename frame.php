b0a34ef237bc452bc82f4fdce6d08ef0ecdd8016
yuanlai <1908153908@qq.com>
2017-05-05 18:49:32 +0800

    changes to be committed:

---
+++ frame.php
@@ -0,0 +1,73 @@
+<?php include 'head.php' ?>
+	<!--最顶颜色栏-->
+	<div class=top></div> 
+	<!--选择栏-->  
+	
+	<ul class="nav nav-tabs">
+	<li class="dropdown">
+		<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+			区县 
+			<span class="caret"></span>
+		</a>
+		<ul class="dropdown-menu">
+			<li><a href="#">谢家集区</a></li>
+			<li><a href="#">大通区</a></li>
+			<li><a href="#">田家庵区</a></li>
+			<li><a href="#">经开区</a></li>
+		</ul>
+	</li>
+	<li class="dropdown">
+		<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+			产业分类 
+			<span class="caret"></span>
+		</a>
+		<ul class="dropdown-menu">
+			<li><a href="#">电子信息和家用电器 </a></li>
+			<li><a href="#">材料和新材料 </a></li>
+			<li><a href="#">汽车和装备制造 </a></li>
+			<li><a href="#">生态环保工程 </a></li>
+		</ul>
+	</li>
+	<li class="dropdown">
+		<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+			投资额 
+			<span class="caret"></span>
+		</a>
+		<ul class="dropdown-menu">
+			<li><a href="#">Swing</a></li>
+			<li><a href="#">jMeter</a></li>
+			<li><a href="#">EJB</a></li>
+			<li class="divider"></li>
+			<li><a href="#">分离的链接</a></li>
+		</ul>
+	</li>
+	<li class="dropdown">
+		<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+			建设阶段 
+			<span class="caret"></span>
+		</a>
+		<ul class="dropdown-menu">
+			<li><a href="#">计划开工项目</a></li>
+			<li><a href="#">续建项目</a></li>
+			<li><a href="#">储备项目</a></li>
+		</ul>
+	</li>
+	
+	<li class="dropdown">
+		<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+			Java <span class="caret"></span>
+		</a>
+		<ul class="dropdown-menu">
+			<li><a href="#">Swing</a></li>
+			<li><a href="#">jMeter</a></li>
+			<li><a href="#">EJB</a></li>
+			<li class="divider"></li>
+			<li><a href="#">分离的链接</a></li>
+		</ul>
+	</li>
+	
+</ul>
+
+
+</body>
+</html>
